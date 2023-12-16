be749628d8151bf5007b84ba9ebec9c6a0b85e9e
srinivasan <srinisubu259@gmail.com>
2023-12-16 14:30:00 +0530

    modified

--- MyClass.java
+++ MyClass.java
@@ -2,7 +2,7 @@ class MyClass {
 
   void add(int x, int y)  //adding two integers 
   {
-<<<<<<< HEAD
+
     System.out.println("Sum of two numbers: " +(x+y));
   }
 
@@ -15,22 +15,17 @@ class MyClass {
   {
     System.out.println("Remainder of of the value : "+(x%y));
   }
-=======
-    System.out.println("Sum of two numbers: " +x+y);
-     
-  }
-<<<<<<< HEAD
-<<<<<<< HEAD
-Void  div(int x, int y)
-{
-System.out.println("Division of two numbers : " x/y);
-}
-Void  div(int a, int b)
-{
-System.out.println("Division of two numbers : " a/b);
 
-=======
-=======
+    System.out.println("Sum of two numbers: " +(x+y));
+
+Void  div (int x, int y)
+{
+System.out.println("Division of two numbers : "+(x/y));
+}
+Void  div(int a, int b,int c)
+{
+System.out.println("Division of two numbers : " +(a+b));
+
 
   void mul(int x,int y)//multiplying two integers
   {
@@ -43,7 +38,7 @@ System.out.println("Division of two numbers : " a/b);
   }
 
 
->>>>>>> 5297243ceb8bea1ed61bc40b3607167e3c18beea
+
   Void Subtraction(int a,int b)
   {
 	  System.out.println("Sum of two numbers: "+ (a-b));
@@ -58,10 +53,5 @@ System.out.println("Division of two numbers : " a/b);
   void add(int a, int b, int c, int d){
      System.out.println("the sum of Four number is : "+(a+b+c+d));
    }
-<<<<<<< HEAD
->>>>>>> f86b7cd2d2c0367e8f165f014011e2acdcc62892
-=======
->>>>>>> 744e27af981391abacf6f0e4b4d6660218e5ded2
 
->>>>>>> 5297243ceb8bea1ed61bc40b3607167e3c18beea
 }

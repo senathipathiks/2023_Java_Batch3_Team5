44a1bf80cf97db7729183d65d2e9d60ff8b1db84
srinivasan <srinisubu259@gmail.com>
2023-12-16 14:13:45 +0530

    Srini modified

--- MyClass.java
+++ MyClass.java
@@ -3,4 +3,15 @@ class MyClass {
   {
     System.out.println("Sum of two numbers: " &x+y);
   }
+
+  void mul(int x,int y)//multiplying two integers
+  {
+    System.out.println("Product of two numbers: "+x*y);
+  }
+  
+  void mul1(int x,int y,int z)//multiplying two integers
+  {
+    System.out.println("Product of two numbers: "+x*y*z);
+  }
+
 }

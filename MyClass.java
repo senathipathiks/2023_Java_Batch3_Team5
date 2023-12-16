3b234bdb412eec818db96910ea133bbaa188d4c9
Sathya <sathyasiva5901@gmail.com>
2023-12-16 14:15:54 +0530

    Remainder methods added

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,18 @@
 class MyClass {
+
   void add(int x, int y)  //adding two integers 
   {
-    System.out.println("Sum of two numbers: " &x+y);
+    System.out.println("Sum of two numbers: " +(x+y));
   }
+
+  void remains(int x,int y) //Remains value
+  {
+    System.out.println("Remains of two Number is : " +(x%y));
+  }
+
+  void Reaminder(int x,int y) //Remainder of the number.
+  {
+    System.out.println("Remainder of of the value : "+(x%y));
+  }
+
 }

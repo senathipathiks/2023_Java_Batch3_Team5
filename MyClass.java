1a5d075ec0864e8cf3d8fa3727504ed2c050e093
sowmiya <sowmiyar678@gmail.com>
2023-12-16 14:13:36 +0530

    MyClass file modified

--- MyClass.java
+++ MyClass.java
@@ -2,5 +2,14 @@ class MyClass {
   void add(int x, int y)  //adding two integers 
   {
     System.out.println("Sum of two numbers: " &x+y);
+     
+  }
+  Void Subtraction(int a,int b)
+  {
+	  System.out.println("Sum of two numbers: " &a-b);
+  }
+  void Subtraction(int c,int d,int e)
+  {
+	  System.out.println("Sum of two numbers: " &c-d-e);
   }
 }

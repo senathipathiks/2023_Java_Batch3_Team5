978b4cbd0e82a16fddfc98d4361b6435ee8b3a79
sundhar raj <sundharachu59@gmail.com>
2023-12-16 14:12:33 +0530

    sundhar commit

--- MyClass.java
+++ MyClass.java
@@ -3,4 +3,10 @@ class MyClass {
   {
     System.out.println("Sum of two numbers: " &x+y);
   }
+  void add(int x, int y, int z){
+	System.out.peintln("sum of three numbers is : "+(x+y+z));
+   }
+  void add(int a, int b, int c, int d){
+     System.out.println("the sum of Four number is : "+(a+b+c+d));
+   }
 }

8e4a38694c435a8676b7181468c572fbdfec5eb5
sundhar raj <sundharachu59@gmail.com>
2023-12-16 14:44:12 +0530

    edited by sundhar

--- MyClass.java
+++ MyClass.java
@@ -1,58 +1,55 @@
 class MyClass {
 
-  void add(int x, int y)  //adding two integers 
+  void add(int x, int y) // adding two integers
   {
 
-    System.out.println("Sum of two numbers: " +(x+y));
+    System.out.println("Sum of two numbers: " + (x + y));
     System.out.println("Edited by Sherin Dhanya");
+    System.out.println("Edited my Sundhar Raj S");
   }
 
-  void remains(int x,int y) //Remains value
+   void add(int x, int y, int z) {
+    System.out.println("sum of three numbers is : " + (x + y + z));
+  }
+
+  void add(int a, int b, int c, int d) {
+    System.out.println("the sum of Four number is : " + (a + b + c + d));
+  }
+
+  void remains(int x, int y) // Remains value
   {
-    System.out.println("Remains of two Number is : " +(x%y));
+    System.out.println("Remains of two Number is : " + (x % y));
   }
 
-  void Reaminder(int x,int y) //Remainder of the number.
+  void Reaminder(int x, int y) // Remainder of the number.
   {
-    System.out.println("Remainder of of the value : "+(x%y));
+    System.out.println("Remainder of of the value : " + (x % y));
   }
 
-    System.out.println("Sum of two numbers: " +(x+y));
+  Void div(int x, int y) {
+    System.out.println("Division of two numbers : " + (x / y));
+  }
 
-Void  div (int x, int y)
-{
-System.out.println("Division of two numbers : "+(x/y));
-}
 Void  div(int a, int b,int c)
 {
 System.out.println("Division of two numbers : " +(a+b));
 
-
-  void mul(int x,int y)//multiplying two integers
+  void mul(int x, int y)// multiplying two integers
   {
-    System.out.println("Product of two numbers: "+(x*y));
-  }
-  
-  void mul1(int x,int y,int z)//multiplying two integers
-  {
-    System.out.println("Product of two numbers: "+(x*y*z));
+    System.out.println("Product of two numbers: " + (x * y));
   }
 
-
-
-  Void Subtraction(int a,int b)
+  void mul1(int x, int y, int z)// multiplying two integers
   {
-	  System.out.println("Sum of two numbers: "+ (a-b));
+    System.out.println("Product of two numbers: " + (x * y * z));
   }
-  void Subtraction(int c,int d,int e)
-  {
-	  System.out.println("Sum of two numbers: " + (c-d-e));
+
+  Void Subtraction(int a, int b) {
+    System.out.println("Sum of two numbers: " + (a - b));
   }
-  void add(int x, int y, int z){
-	System.out.println("sum of three numbers is : "+(x+y+z));
-   }
-  void add(int a, int b, int c, int d){
-     System.out.println("the sum of Four number is : "+(a+b+c+d));
-   }
+
+  void Subtraction(int c, int d, int e) {
+    System.out.println("Sum of two numbers: " + (c - d - e));
+  }
 
 }
